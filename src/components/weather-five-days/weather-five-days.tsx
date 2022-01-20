d1ac665c79caf38c8f6146c9fc6da0b3190e5af8
IliyanIvovMarkov <i.markov@allfront.io>
2022-01-20 13:50:31 +0200

    Clean up weather initial commit

--- src/components/weather-five-days/weather-five-days.tsx
+++ src/components/weather-five-days/weather-five-days.tsx
@@ -3,38 +3,43 @@ import React, {useEffect, useState} from 'react';
 import './weather-five-days.css';
 import {DayForecast} from "../day-forecast/day-forecast";
 
+let today = new Date();
+
+const getTemp = (tempList: any) => {
+  return (tempList.reduce((prevValue: number, curValue: number) => prevValue + curValue) / 5).toFixed();
+}
+
+const mapApiData = (apiData: any) => {
+  let listOfTemp = apiData.list.map((everyThreeHourData: any) => Number(everyThreeHourData.main.temp.toFixed()));
+
+  return {
+    day: {
+      one: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate(),
+      two: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate() + 1),
+      three: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate() + 2),
+      four: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate() + 3),
+      five: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate() + 4)
+    },
+    temp: {
+      one: getTemp(listOfTemp.slice(0, 8)),
+      two: getTemp(listOfTemp.slice(8, 16)),
+      three: getTemp(listOfTemp.slice(16, 24)),
+      four: getTemp(listOfTemp.slice(24, 32)),
+      five: getTemp(listOfTemp.slice(32))
+    },
+    weather: {
+      one: apiData.list[2].weather[0].main,
+      two: apiData.list[10].weather[0].main,
+      three: apiData.list[18].weather[0].main,
+      four: apiData.list[26].weather[0].main,
+      five: apiData.list[34].weather[0].main
+    }
+  };
+};
+
 export const WeatherFiveDays = (props: any) => {
-  let today = new Date();
   const  [fiveDayData, setFiveDayData]: any   = useState(null)
   const  [isLoading, setIsLoading]: any   = useState(true)
-
-  const mapApiData = (apiData: any) => {
-    let listOfTemp = apiData.list.map((everyThreeHourData: any) => Number(everyThreeHourData.main.temp.toFixed()));
-
-    return {
-      day: {
-        one: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate(),
-        two: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate() + 1),
-        three: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate() + 2),
-        four: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate() + 3),
-        five: today.getFullYear()+'-'+(today.getMonth()+1)+'-'+(today.getDate() + 4)
-      },
-      temp: {
-        one: (listOfTemp.slice(0, 8).reduce((prevValue: number, curValue: number) => prevValue + curValue) / 5).toFixed(),
-        two: (listOfTemp.slice(8, 16).reduce((prevValue: number, curValue: number) => prevValue + curValue) / 5).toFixed(),
-        three: (listOfTemp.slice(16, 24).reduce((prevValue: number, curValue: number) => prevValue + curValue) / 5).toFixed(),
-        four: (listOfTemp.slice(24, 32).reduce((prevValue: number, curValue: number) => prevValue + curValue) / 5).toFixed(),
-        five: (listOfTemp.slice(32).reduce((prevValue: number, curValue: number) => prevValue + curValue) / 5).toFixed()
-      },
-      weather: {
-        one: apiData.list[2].weather[0].main,
-        two: apiData.list[10].weather[0].main,
-        three: apiData.list[18].weather[0].main,
-        four: apiData.list[26].weather[0].main,
-        five: apiData.list[34].weather[0].main
-      }
-    };
-  };
 
   useEffect(() => {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&units=metric&appid=${props.apiKey}`)
@@ -45,6 +50,7 @@ export const WeatherFiveDays = (props: any) => {
         setIsLoading(false)
       })
   }, []);
+
 
   return (
     <>
